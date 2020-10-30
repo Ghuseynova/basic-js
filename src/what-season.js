6cac8337d98b5302a6fe51b696b6c686062bdd6e
Gunel Huseynova <gorellik246@gmail.com>
2020-10-30 15:34:07 +0300

    refactor: refactor getSeason func

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+
+  if(!arguments.length){
+    return 'Unable to determine the time of year!'
+  } 
+
+    if(checkDate(date)) {
+      const month = date.getMonth();
+    
+      switch(month.toString()) {
+        case '11':
+        case '0':
+        case '1':
+          
+          return 'winter';
+        case '2':
+        case '3':
+        case '4':
+          return 'spring';
+        case '5':
+        case '6':
+        case '7':
+          return 'summer';
+        case '8':
+        case '9':
+        case '10':
+          return 'autumn';
+      }
+    } else {
+      throw new Error()
+    }
+    
 };
+
+function checkDate(date) {
+  return Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date);
+}
+
